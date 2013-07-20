16ebdfbd416e56d485b3f9ace71302daba34d90f
Cameron Jordan <cjordan@cameronism.com>
2013-07-20 00:03:56 -0700

    Bits, moar bits

--- src/CoderBattle/Scripts/coderbattle.js
+++ src/CoderBattle/Scripts/coderbattle.js
@@ -18,13 +18,58 @@ function setPageHash() {
 	}
 }
 
+function getBits(number, reverse) {
+	var s = number.toString(2);
+
+	if (reverse) {
+		s = s.split('').reverse().join('');
+	}
+
+	return s.replace(/\d/g, function (flag) {
+		return '<span class=bit' + flag + '></span>';
+	});
+}
+
+function showBout($header, items) {
+
+	items.forEach(function (item) {
+		var className,
+			value,
+			$cell,
+			$row = $(
+				'<div class="row">' +
+					'<div class="Fighter1Hits span3 offset2"></div>' +
+					'<div class="Message span2"></div>' +
+					'<div class="Fighter2Hits span3"></div>' +
+				'</div>');
+
+		$header.after($row);
+
+		// dirty - let the classnames match
+		for (className in item) {
+			value = item[className];
+			if (value === 0) continue;
+
+			$cell = $row.find('.' + className);
+
+			if (className == 'Message') {
+				$cell.text(value);
+			} else {
+				$cell.html(getBits(value, className == 'Fighter2Hits'));
+			}
+		}
+		 
+	});
+}
+
+
 // brawler init
 $('.brawler .init').on('keyup', function (e) {
 	if (e.keyCode != 13) return;
 	var el = $(this),
 		name = el.val();
 
-	trigger(el, 'b_setName', name)
+	trigger(el, 'b_setName', name);
 	//el.closest('.brawler').trigger();
 });
 
@@ -34,13 +79,21 @@ $('.brawler').on({
 		var url = 'http://www.gravatar.com/avatar/' + hash + '?d=mm&s=256';
 		console.log(url);
 
-		$b.html('<a target=_blank href="' + link + '"><img src="' + url + '"></a>')
+		$b.html('<a target=_blank href="' + link + '"><img src="' + url + '"></a>');
 	},
 	b_setProfile: function (e, profile) {
 		$b = $(this);
 		$b.data(profile);
-		trigger($b, 'b_getGravatar', profile.gravatar_hash, profile.link);
-		setPageHash();
+
+		var gravatar = profile.gravatar_hash,
+			link = profile.link;
+
+		if (gravatar && link) {
+			trigger($b, 'b_getGravatar', gravatar, link);
+			setPageHash();
+		} else {
+			$b.find('label').append('<div class=error>not found</div>');
+		}
 	},
 	b_setName: function (e, name) {
 		console.log(e.name, name);
@@ -53,17 +106,25 @@ $('.brawler').on({
 		var $b = $(this);
 
 		$.getJSON('https://coderbits.com/' + name + '.json?callback=?').then(function (data) {
-	//		console.log(data);
+			//		console.log(data);
 			trigger($b, 'b_setProfile', data);
-		})
+		});
 	}
 });
 
 $(document).ready(function () {
 	var fightHubProxy = $.connection.fightHub;
 
-    fightHubProxy.client.boutComplete = function (boutResult) {
-        console.log(boutResult);
+	fightHubProxy.client.boutComplete = function (boutResult) {
+		console.log(boutResult);
+		var category = boutResult.Category,
+			items = boutResult.Results,
+			header = $('<h3>' + _.escape(category) + '</h3>');
+
+		// change `after` to `before` to have newest at the bottom
+		$('#results').after(header);
+
+		showBout(header, items);
 	};
 
 	//Start the signalr hub
